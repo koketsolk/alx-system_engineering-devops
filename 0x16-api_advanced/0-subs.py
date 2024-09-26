da4d731abef8391b57f74a491699091f096b1b44
koketsolk <koketsolekau86@gmail.com>
2024-09-26 15:58:47 -0700

    2correcto

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -24,22 +24,4 @@ def number_of_subscribers(subreddit):
         return 0
     if 'subscribers' not in dic.get('data'):
         return 0
-    return res.json()['data']['subscribers']#!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+    return res.json()['data']['subscribers']
